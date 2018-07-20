e9c033e6619f68c9c80b2417166dfe21252f4a55
PresentKim <qjrmckd@gmail.com>
2018-07-20 13:08:36 +0900

    Init userscript

---
+++ bandCopyPost.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Band CopyPost by AdGuard
+// @namespace AdGuard
+// @description Show hidden "Copy post to other band" option in BAND
+// @description:ko BAND에서 숨겨진 "다른 밴드에 올리기" 옵션을 표시합니다
+// @version 0.0.1-DEV
+// @license LGPL-3.0; https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/LICENSE
+// @downloadURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.user.js
+// @updateURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.meta.js
+// @supportURL https://github.com/PresentKim/BandCopyPost-AdGuard/issues
+// @homepageURL https://github.com/PresentKim/BandCopyPost-AdGuard
+// @include *
+// @run-at document-start
+// ==/UserScript==
+console.log("[Band CopyPost] Init Band CopyPost by AdGuard");
+
+window.addEventListener("load", function(){
+    console.log("[Band CopyPost] Run Band CopyPost");
+});
