32bcbbca495ff6c3e67163cfefa735e4cd1f070b
PresentKim <qjrmckd@gmail.com>
2018-07-20 15:44:31 +0900

    Set background color of added option (to LightGreen)
    https://www.w3schools.com/colors/color_tryit.asp?color=LightGreen

--- bandCopyPost.user.js
+++ bandCopyPost.user.js
@@ -3,7 +3,7 @@
 // @namespace AdGuard
 // @description Show hidden "Copy post to other band" option in BAND
 // @description:ko BAND에서 숨겨진 "다른 밴드에 올리기" 옵션을 표시합니다
-// @version 0.0.10-DEV
+// @version 0.0.11-DEV
 // @license LGPL-3.0; https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/LICENSE
 // @downloadURL https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/bandCopyPost.user.js?raw=true
 // @updateURL https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/bandCopyPost.meta.js?raw=true
@@ -13,7 +13,7 @@
 // @match https://band.us/*
 // @run-at document-start
 // ==/UserScript==
-console.log("[Band CopyPost] Init Band CopyPost by AdGuard v0.0.10-DEV");
+console.log("[Band CopyPost] Init Band CopyPost by AdGuard v0.0.11-DEV");
 
 window.addEventListener("load", function () {
     console.log("[Band CopyPost] Run Band CopyPost");
@@ -40,6 +40,7 @@ window.addEventListener("load", function () {
 
                             var li = document.createElement("li");
                             li.appendChild(a);
+                            li.style.backgroundColor = "#90EE90";
                             optionButtons[j].nextSibling.firstChild.appendChild(li);
                         }
                     }
