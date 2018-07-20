953a61882e684c2eeb94242b54b901cbb0b4fa39
PresentKim <qjrmckd@gmail.com>
2018-07-20 13:18:52 +0900

    Try @math option

--- bandCopyPost.meta.js
+++ bandCopyPost.meta.js
@@ -3,12 +3,13 @@
 // @namespace AdGuard
 // @description Show hidden "Copy post to other band" option in BAND
 // @description:ko BAND에서 숨겨진 "다른 밴드에 올리기" 옵션을 표시합니다
-// @version 0.0.2-DEV
+// @version 0.0.3-DEV
 // @license LGPL-3.0; https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/LICENSE
 // @downloadURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.user.js
 // @updateURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.meta.js
 // @supportURL https://github.com/PresentKim/BandCopyPost-AdGuard/issues
 // @homepageURL https://github.com/PresentKim/BandCopyPost-AdGuard
-// @include *://band.us/*
+// @match http://band.us/*
+// @match https://band.us/*
 // @run-at document-start
 // ==/UserScript==
