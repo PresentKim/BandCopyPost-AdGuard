7c6f7667cee33377afe84259b5b7f8515305ab47
PresentKim <qjrmckd@gmail.com>
2018-07-20 13:20:24 +0900

    Fix @downloadURL and @updateURL

--- bandCopyPost.meta.js
+++ bandCopyPost.meta.js
@@ -5,8 +5,8 @@
 // @description:ko BAND에서 숨겨진 "다른 밴드에 올리기" 옵션을 표시합니다
 // @version 0.0.3-DEV
 // @license LGPL-3.0; https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/LICENSE
-// @downloadURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.user.js
-// @updateURL https://raw.githubusercontent.com/PresentKim/BandCopyPost-AdGuard/master/bandCopyPost.meta.js
+// @downloadURL https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/bandCopyPost.user.js?raw=true
+// @updateURL https://github.com/PresentKim/BandCopyPost-AdGuard/blob/master/bandCopyPost.meta.js?raw=true
 // @supportURL https://github.com/PresentKim/BandCopyPost-AdGuard/issues
 // @homepageURL https://github.com/PresentKim/BandCopyPost-AdGuard
 // @match http://band.us/*
